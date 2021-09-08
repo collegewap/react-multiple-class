0528220fe956e76d86f29e53da6fef6235c06ea1
Abhishek EH <collegewap@gmail.com>
2021-09-08 17:58:46 +0530

    using props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
+const ChildComp = (props) => {
+  return <div className={`box ${props.status}`}>This is an error message</div>;
+};
+
 function App() {
   const [isWarning] = useState(true);
   return (
     <>
       <div className="box success">This is a success message</div>
       <div className={`box ${isWarning === true && "warning"}`}>
-        This is a success message
+        This is a warning message
       </div>
+      <ChildComp status="error" />
     </>
   );
 }
